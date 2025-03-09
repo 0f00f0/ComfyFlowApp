b63345c1f2d683831959c7a180b97eaf90b6e4c1
0f00f0 <148326122+0f00f0@users.noreply.github.com>
2025-03-09 20:29:35 +0800

    Update creator_run.sh

--- bin/creator_run.sh
+++ bin/creator_run.sh
@@ -11,7 +11,7 @@ export LOGURU_LEVEL=INFO
 
 export STREAMLIT_SERVER_PORT=8501
 # set COMFYFLOW_API_URL to comfyflow api url
-export COMFYFLOW_API_URL=http://38.207.176.168/
+export COMFYFLOW_API_URL=http://127.0.0:9009
 # set COMFYUI_SERVER_ADDR to comfyui server addr
 export COMFYUI_SERVER_ADDR=http://42.192.108.200:6889/
 # set MODE to Creator
